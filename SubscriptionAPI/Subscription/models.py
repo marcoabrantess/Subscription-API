0248cb7c4ee6c76c41a6b210ba30fa14326eecbd
Marco Aurélio <marco.abrantes@ufv.br>
2025-06-15 11:45:44 -0300

    Fix models and admins

--- SubscriptionAPI/Subscription/models.py
+++ SubscriptionAPI/Subscription/models.py
@@ -1,7 +1,16 @@
 from django.db import models
+from SubscriptionPlan.models import SubscriptionPlan
+from Customer.models import Customer
 
 class Subscription(models.Model):
-    name = models.CharField(max_length=255)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    description = models.TextField()
-    billing_interval = models.CharField(max_length=50)
+    plan = models.ForeignKey(SubscriptionPlan, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    stripe_subscription_id = models.CharField(max_length=255, blank=True, null=True)
+
+    STATUS_CHOICES = [
+        ('active', 'Active'),
+        ('canceled', 'Canceled'),
+        ('trialing', 'Trialing'),
+    ]
+    status = models.CharField(max_length=20,choices=STATUS_CHOICES)
+    
--- SubscriptionAPI/SubscriptionPlan/models.py
+++ SubscriptionAPI/SubscriptionPlan/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class SubscriptionPlan(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    billing_interval = models.CharField(max_length=50)
